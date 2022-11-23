298e5779ea10a80b09ac83ca0e85f069a4ba0708
AegisVP <vlad@pysarenko.com>
2022-11-23 01:41:18 +0100

    some more error checking Auth header

--- middlewares/authService.js
+++ middlewares/authService.js
@@ -3,22 +3,24 @@ const { User } = require('../model');
 const { requestError } = require('../utils');
 
 async function authService(req, res, next) {
-    if (!req?.headers['authorization']) return next(requestError(401, 'No Authorization header', 'NoAuthHeader'));
+  if (!req?.headers['authorization']) return next(requestError(401, 'No Authorization header', 'NoAuthHeader'));
 
-    const [authType, token] = req.headers['authorization'].split(' ');
-    const decodedUser = jwt.decode(token);
+  const [authScheme, token] = req.headers['authorization'].split(' ');
+  if (authScheme !== 'Bearer') return next(requestError(401, 'Auth scheme unsupported', 'InvalidHeader'));
+  if (!token) return next(requestError(401, 'Token not valid', 'InvalidHeader'));
 
-    const dbUser = await User.findById(decodedUser._id);
-    if (!dbUser) return next(requestError(401, 'Token not valid', 'NoTokenUser'));
+  const decodedUser = jwt.decode(token);
+  const dbUser = await User.findById(decodedUser._id);
+  if (!dbUser) return next(requestError(401, 'Token not valid', 'NoTokenUser'));
 
-    if (dbUser.token !== token) {
-      await User.findByIdAndUpdate(dbUser._id, { token: '' });
-      return next(requestError(401, 'Token not valid', 'TokenMismatch'));
-    }
+  if (dbUser.token !== token) {
+    await User.findByIdAndUpdate(dbUser._id, { token: '' });
+    return next(requestError(401, 'Token not valid', 'TokenMismatch'));
+  }
 
-    req.user = dbUser;
+  req.user = dbUser;
 
-    return next();
+  return next();
 }
 
 module.exports = { authService };
